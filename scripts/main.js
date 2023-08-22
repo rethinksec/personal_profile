9fc2975ca2dab6503702dc7a42504be7ff0c85ac
CY Teng <cheeyuen.teng@owasp.org>
2023-08-22 22:28:57 +0800

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 // Animations
 AOS.init({
-  anchorPlacement: 'top-left',
-  duration: 1000
+	anchorPlacement: 'top-left',
+	duration: 1000
 });
 
 // Add your javascript here
+
+function current_age() {
+	today = new Date();
+	this_year = today.getFullYear();
+	age = this_year - 1996 + 1;
+	document.getElementById("age").innerHTML = age;
+}
